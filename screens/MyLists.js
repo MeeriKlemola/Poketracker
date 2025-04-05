be1d87decb5226a0dc4be3b9d80675e88e2e3e6d
MeeriKlemola <bhi057@myy.haaga-helia.fi>
2025-04-05 13:48:44 +0300

    MyLists added, completely empty

---
+++ screens/MyLists.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Here are your lists, when I get around doing them.</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
