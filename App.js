e066c8751328328f29851602ee012b85bcf075f3
MeeriKlemola <bhi057@myy.haaga-helia.fi>
2025-04-05 13:33:56 +0300

    basic tabnavigation

--- App.js
+++ App.js
@@ -1,17 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { AntDesign } from '@expo/vector-icons';
+import HomeScreen from "./screens/HomeScreen";
+import MyLists from "./screens/MyLists";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => {
+
+            if (route.name === 'Home') {
+              return <Ionicons name='home' size={size} color={color} />;
+
+            } else if (route.name === 'My Lists') {
+              return <AntDesign name="book" size={size} color={color} />;
+            }
+
+          },
+        })}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="My Lists" component={MyLists} />
+
+      </Tab.Navigator>
+    </NavigationContainer>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 40,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
