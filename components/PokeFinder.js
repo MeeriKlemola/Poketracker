7f7b998d2d33d0b5cda21299bd2bbfb68c24e97d
MeeriKlemola <bhi057@myy.haaga-helia.fi>
2025-04-05 13:51:35 +0300

    added pokemon search function

---
+++ components/PokeFinder.js
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import { View, TextInput, Button, StyleSheet, Alert, ActivityIndicator } from 'react-native';
+import PokeList from './PokeList.js';
+import { getPokemon } from '../api.js';
+
+export default function PokeFinder() {
+
+    const [keyword, setKeyword] = useState("");
+    const [pokemon, setPokemon] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const handleFetch = () => {
+        if (!keyword) {
+            Alert.alert("Warning", "Enter a pokemon first");
+        }
+        else {
+            setLoading(true);
+            getPokemon(keyword)
+                .then(data => setPokemon([data]))
+                .catch(err => console.error(err))
+                .finally(() => setLoading(false));
+        } }
+
+        return (
+            <View style={styles.inputForm}>
+                <TextInput
+                    placeholder='Enter a pokemon'
+                    value={keyword}
+                    onChangeText={text => setKeyword(text)}
+                />
+
+                <Button
+                    title='Search'
+                    onPress={handleFetch}
+                />
+                {
+                    loading ? <ActivityIndicator size="large" /> :
+                        <PokeList pokemon={pokemon} />
+                }
+            </View>
+        )
+    }
+
+const styles = StyleSheet.create({
+    inputForm: {
+        flex: 1,
+    },
+    listStyle: {
+        flex: 8,
+    }
+})
