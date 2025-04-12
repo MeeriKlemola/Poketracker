92aff8a41685ac518091f2063f0a528f6058238f
MeeriKlemola <bhi057@myy.haaga-helia.fi>
2025-04-12 23:35:46 +0300

    add settings screen

---
+++ screens/Settings.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, Modal, TextInput } from 'react-native';
+
+export default function App () {
+
+    return (
+        <>
+        <Text>Here are your settings.</Text>
+        <StatusBar style="auto" />
+        </>
+    )
+
+}
