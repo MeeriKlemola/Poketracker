955c1690a091230a7d228d48256fe8860f652eb5
MeeriKlemola <bhi057@myy.haaga-helia.fi>
2025-05-01 01:34:44 +0300

    add displays now the pokemon inside the list

--- components/ListofLists.js
+++ components/ListofLists.js
@@ -1,12 +1,17 @@
 import { View, Text } from "react-native"
 
-export default function ListofLists({listNames}) {
+export default function ListofLists({ lists }) {
 
-    return (
-        <View>
-        {listNames.map((name, index) => (
-          <Text key={index}> {name}</Text>
-        ))}
-      </View>
-    )
+  return (
+    <View>
+      {Object.entries(lists).map(([listName, pokemons]) => (
+        <View key={listName}>
+          <Text style={{ fontWeight: 'bold' }}>{listName}</Text>
+          {pokemons.map(p => (
+            <Text key={p.id}>- {p.name}</Text>
+          ))}
+        </View>
+      ))}
+    </View>
+  )
 }
