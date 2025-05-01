99151b0baa49ee2efe6ab86d444d70e17cb974e9
MeeriKlemola <bhi057@myy.haaga-helia.fi>
2025-05-01 23:44:50 +0300

    add error handling to apiGetPokemon.js

--- apiGetPokemon.js
+++ apiGetPokemon.js
@@ -2,9 +2,13 @@ export function getPokemon(keyword) {
 
     return fetch(`https://pokeapi.co/api/v2/pokemon/${keyword}`)
         .then(response => {
-            if (!response.ok)
-                throw new Error("Error in fetch:" + response.statusText);
-
+            if (!response.ok) {
+                if (response.status === 404) {
+                    throw new Error("Pokémon not found. Check name.");
+                } else {
+                    throw new Error(`Error in fetch: ${response.status} ${response.statusText}`);
+                }
+            }
             return response.json();
         })
 }
