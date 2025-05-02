4b50d25ab00ecd532e9beb3a10d6ee6f36c26a26
MeeriKlemola <bhi057@myy.haaga-helia.fi>
2025-05-02 13:27:04 +0300

    update styles in the ListofLists.js

--- components/ListofLists.js
+++ components/ListofLists.js
@@ -1,17 +1,48 @@
-import { View, Text } from "react-native"
+import { View, Text, ScrollView, StyleSheet } from "react-native"
 
 export default function ListofLists({ lists }) {
 
   return (
-    <View>
+    <ScrollView contentContainerStyle={styles.container}>
+
       {Object.entries(lists).map(([listName, pokemons]) => (
-        <View key={listName}>
-          <Text style={{ fontWeight: 'bold' }}>{listName}</Text>
-          {pokemons.map(p => (
-            <Text key={p.id}>- {p.name}</Text>
-          ))}
+        
+        <View key={listName} style={styles.listContainer}>
+
+          <Text style={styles.listTitle}>{listName}</Text>
+          <Text style={styles.countText}>
+            {pokemons.length} Pokémon{pokemons.length !== 1 }
+          </Text>
+
         </View>
       ))}
-    </View>
+    </ScrollView>
   )
 }
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    marginTop: 45,
+    alignItems:'center',
+  },
+  listContainer: {
+    backgroundColor: '#f0f4f7',
+    marginBottom: 16,
+    padding: 12,
+    borderRadius: 10,
+    elevation: 2,
+    width: '100%',
+    alignSelf: 'center',
+  },
+  listTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 8,
+    color: '#333',
+  },
+  countText: {
+    fontStyle: 'italic',
+    color: '#aaa',
+    paddingLeft: 5,
+  }
+});
