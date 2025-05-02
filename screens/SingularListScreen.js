d8300889c033719b005469d6c171363dd7ab5073
MeeriKlemola <bhi057@myy.haaga-helia.fi>
2025-05-02 21:36:36 +0300

    modify now displays pokemons names that are in the list to SingularListScreen.js

--- screens/SingularListScreen.js
+++ screens/SingularListScreen.js
@@ -1,18 +1,43 @@
 import React from 'react';
-import { FlatList, View, Text } from "react-native"
+import { FlatList, View, Text, Button, StyleSheet, TouchableOpacity, Image } from "react-native"
 
-export default function SingularListScreen({ listName }) {
+export default function SingularListScreen({ route, navigation }) {
+    const { listName, pokemons } = route.params;
 
     return (
-        <View>
-            {Object.entries(listName).map(([pokemons]) => (
-                <View key={listName}>
-                    <Text style={{ fontWeight: 'bold' }}>{listName}</Text>
-                    {pokemons.map(p => (
-                        <Text key={p.id}>- {p.name}</Text>
-                    ))}
-                </View>
+        <View style={styles.container}>
+            <Text style={{ fontSize: 24, marginBottom: 5, fontWeight: 'bold'}}>{listName}</Text>
 
-            ))}
-        </View>)
-}  
+            <FlatList
+                data={pokemons}
+                keyExtractor={(item, index) => `${item.name}-${index}`}
+                renderItem={({ item }) => (
+                    <View>
+                        <Text style={styles.text}>{item.name.charAt(0).toUpperCase() + item.name.slice(1).toLowerCase()}</Text>   
+                    </View>
+                )}
+            />
+
+            <Button title="Add a Pokémon" onPress={() => {/* Lisää toiminto kunhan pystyt */ }} />
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        padding: 10,
+        marginTop: 45,
+    },
+    listContainer: {
+        backgroundColor: '#f0f4f7',
+        marginBottom: 16,
+        padding: 12,
+        borderRadius: 10,
+        elevation: 2,
+        width: '100%',
+        alignSelf: 'center',
+    },
+    text: {
+        paddingVertical: 8,
+        fontSize: 20,
+    }
+})
