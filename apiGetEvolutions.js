f20dc62ab5ee1939468459161647e52f2ea07ae6
MeeriKlemola <bhi057@myy.haaga-helia.fi>
2025-05-03 11:23:50 +0300

    delete evolution files, as the api didn't offer a link between one pokemon and it's evolutions"

--- apiGetEvolutions.js
+++
@@ -1,8 +0,0 @@
-export async function getEvolutions(id) {
-    const response = await fetch(`https://pokeapi.co/api/v2/evolution-chain/${id}/`);
-    if (!response.ok) {
-      throw new Error("Error in fetch: " + response.statusText);
-    }
-    return await response.json();
-  }
-
